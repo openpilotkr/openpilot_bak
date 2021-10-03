862b9bbbd234a6517f736d3df918cc402f471757
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-04 00:11:57 +0900

    kyd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -975,9 +975,9 @@ static void draw_safetysign(UIState *s) {
   const int s_center_y = bdr_s + 105;
   
   const int d_center_x = s_center_x;
-  const int d_center_y = s_center_y + 160;
-  const int d_width = 230;
-  const int d_height = 80;
+  const int d_center_y = s_center_y + 155;
+  const int d_width = 220;
+  const int d_height = 70;
   int opacity = 0;
 
   const Rect rect_s = {s_center_x - diameter/2, s_center_y - diameter/2, diameter, diameter};
@@ -998,23 +998,23 @@ static void draw_safetysign(UIState *s) {
   opacity = safety_dist>1020 ? 0 : (1020 - safety_dist) * 0.25;
 
   if (safety_speed > 29 && !s->scene.comma_stock_ui) {
-    ui_fill_rect(s->vg, rect_s, COLOR_WHITE_ALPHA(200), diameter/2 - 15);
+    ui_fill_rect(s->vg, rect_s, COLOR_WHITE_ALPHA(200), diameter/2 + 15);
     ui_draw_rect(s->vg, rect_s, COLOR_RED_ALPHA(200), 30, diameter/2);
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
     if (safety_speed < 100) {
-      ui_draw_text(s, rect_s.centerX(), rect_s.centerY(), safetySpeed, 130, COLOR_BLACK_ALPHA(200), "sans-bold");
+      ui_draw_text(s, rect_s.centerX(), rect_s.centerY(), safetySpeed, 120, COLOR_BLACK_ALPHA(200), "sans-bold");
     } else {
-      ui_draw_text(s, rect_s.centerX(), rect_s.centerY(), safetySpeed, 110, COLOR_BLACK_ALPHA(200), "sans-bold");
+      ui_draw_text(s, rect_s.centerX(), rect_s.centerY(), safetySpeed, 100, COLOR_BLACK_ALPHA(200), "sans-bold");
     }
     ui_fill_rect(s->vg, rect_d, COLOR_ORANGE_ALPHA(opacity), 20.);
     ui_draw_rect(s->vg, rect_d, COLOR_WHITE_ALPHA(200), 10, 20);
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
     ui_draw_text(s, rect_d.centerX(), rect_d.centerY(), safetyDist, 70, COLOR_WHITE_ALPHA(200), "sans-bold");
   } else if ((s->scene.mapSign == 195 || s->scene.mapSign == 197) && safety_speed == 0 && safety_dist != 0 && !s->scene.comma_stock_ui) {
-    ui_fill_rect(s->vg, rect_s, COLOR_WHITE_ALPHA(200), diameter/2 - 15);
+    ui_fill_rect(s->vg, rect_s, COLOR_WHITE_ALPHA(200), diameter/2 + 15);
     ui_draw_rect(s->vg, rect_s, COLOR_RED_ALPHA(200), 30, diameter/2);
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-    ui_draw_text(s, rect_s.centerX(), rect_s.centerY(), "가변\n구간", 100, COLOR_BLACK_ALPHA(200), "sans-bold");
+    ui_draw_text(s, rect_s.centerX(), rect_s.centerY(), "가변\n구간", 90, COLOR_BLACK_ALPHA(200), "sans-bold");
 
     ui_fill_rect(s->vg, rect_d, COLOR_ORANGE_ALPHA(opacity), 20.);
     ui_draw_rect(s->vg, rect_d, COLOR_WHITE_ALPHA(200), 10, 20);
