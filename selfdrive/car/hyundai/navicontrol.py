248e73b6d022305e6eff74809774c5e63363198c
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-04 14:23:44 +0900

    kyd

--- selfdrive/car/hyundai/navicontrol.py
+++ selfdrive/car/hyundai/navicontrol.py
@@ -5,11 +5,14 @@ import numpy as np
 from selfdrive.config import Conversions as CV
 from selfdrive.car.hyundai.values import Buttons
 from common.numpy_fast import clip, interp
+from cereal import log
 import cereal.messaging as messaging
 from common.params import Params
 
 import common.log as trace1
 import common.MoveAvg as mvAvg
+
+LaneChangeState = log.LateralPlan.LaneChangeState
 
 class NaviControl():
   def __init__(self, p=None):
@@ -218,19 +221,21 @@ class NaviControl():
 
   def auto_speed_control(self, CS, ctrl_speed, path_plan):
     modelSpeed = path_plan.modelSpeed
+    min_control_speed = 20 if CS.is_set_speed_in_mph else 30
     if CS.gasPressed == self.gasPressed_old:
       return ctrl_speed
     elif self.gasPressed_old:
       clu_Vanz = CS.clu_Vanz
-      ctrl_speed = max(ctrl_speed, clu_Vanz)
+      ctrl_speed = max(min_control_speed, ctrl_speed, clu_Vanz)
       CS.set_cruise_speed(ctrl_speed)
     elif CS.CP.resSpeed:
-      ctrl_speed = CS.CP.resSpeed
+      ctrl_speed = max(min_control_speed, CS.CP.resSpeed)
     elif CS.cruise_set_mode in [1,2,4]:
       vFuture = CS.CP.vFuture
-      ctrl_speed = vFuture
+      ctrl_speed = max(min_control_speed, vFuture)
 
-    if CS.cruise_set_mode in [1,3,4] and CS.out.vEgo * CV.MS_TO_KPH > 40 and modelSpeed < 90:
+    if CS.cruise_set_mode in [1,3,4] and CS.out.vEgo * CV.MS_TO_KPH > 40 and modelSpeed < 90 and \
+     path_plan.laneChangeState == LaneChangeState.off and not ( CS.out.leftBlinker or CS.out.rightBlinker):
       ctrl_speed = min(ctrl_speed, interp(modelSpeed, [30, 90], [45, 90])) # curve speed ratio
 
     self.gasPressed_old = CS.gasPressed
