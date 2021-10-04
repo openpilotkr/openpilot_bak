610f5cfdf8ab1b73101f2ebef0525ce201863c94
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-04 11:59:21 +0900

    kyd

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -435,6 +435,9 @@ QWidget * network_panel(QWidget * parent) {
 UserPanel::UserPanel(QWidget* parent) : QWidget(parent) {
   QVBoxLayout *layout = new QVBoxLayout(this);
 
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->setSpacing(30);
+
   // OPKR
   layout->addWidget(new LabelControl("UI설정", ""));
   layout->addWidget(new AutoShutdown());
@@ -547,6 +550,9 @@ UserPanel::UserPanel(QWidget* parent) : QWidget(parent) {
 
 TuningPanel::TuningPanel(QWidget* parent) : QWidget(parent) {
   QVBoxLayout *layout = new QVBoxLayout(this);
+
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->setSpacing(30);
 
   // OPKR
   layout->addWidget(new LabelControl("튜닝메뉴", ""));
