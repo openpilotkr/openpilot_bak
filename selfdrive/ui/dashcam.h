e02319fa8537b01821d6793dd74b60d7d15c7212
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-08 00:54:48 +0900

    kyd

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -279,7 +279,7 @@ void dashcam(UIState *s) {
     if (click_elapsed_time > 0) {
       click_time = get_time() + 1;
       screen_toggle_record_state(s);
-      print("%d", s->scene.touched);
+      printf("touched=%d,  clicktime=%d", s->scene.touched, click_time);
       s->scene.touched = false;
     }
   } else if (s->scene.touched) {
