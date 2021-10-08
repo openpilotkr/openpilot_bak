d3975264ddffe89519c250a8d9b07332b6b02fae
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-08 17:39:17 +0900

    kyd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1041,9 +1041,9 @@ static void draw_compass(UIState *s) {
     nvgTranslate(s->vg, compass_x+compass_size/2, compass_y+compass_size/2);
     nvgRotate(s->vg, -niddle_rotation);
     nvgFontFace(s->vg, "sans-bold");
-    nvgFontSize(s->vg, 65);
+    nvgFontSize(s->vg, 60);
     nvgFillColor(s->vg, COLOR_WHITE_ALPHA(200));
-    nvgText(s->vg, 0, 0, "←", NULL);
+    nvgText(s->vg, 0, 0, "↑", NULL);
     nvgRestore(s->vg);
   }
 }
