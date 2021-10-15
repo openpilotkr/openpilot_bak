a215b138e2d66774646b305c6d03aedb20d5bf65
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-15 15:34:26 +0900

    comma

---
+++ selfdrive/ui/qt/widgets/opkr.cc
@@ -0,0 +1,89 @@
+#include "selfdrive/ui/qt/widgets/opkr.h"
+
+#include <QHBoxLayout>
+#include <QTextStream>
+#include <QFile>
+
+#include "selfdrive/common/params.h"
+#include "selfdrive/ui/qt/widgets/input.h"
+
+CarSelectCombo::CarSelectCombo() : AbstractControl("", "", "") 
+{
+  combobox.setStyleSheet(R"(
+    subcontrol-origin: padding;
+    subcontrol-position: top left;
+    selection-background-color: #111;
+    selection-color: yellow;
+    color: white;
+    background-color: #393939;
+    border-style: solid;
+    border: 0px solid #1e1e1e;
+    border-radius: 0;
+    width: 100px;
+  )");
+
+  combobox.addItem("Select Your Car");
+  QFile carlistfile("/data/params/d/CarList");
+  if (carlistfile.open(QIODevice::ReadOnly)) {
+    QTextStream carname(&carlistfile);
+    while (!carname.atEnd()) {
+      QString line = carname.readLine();
+      combobox.addItem(line);
+    }
+    carlistfile.close();
+  }
+
+  combobox.setFixedWidth(1055);
+
+  btn.setStyleSheet(R"(
+    padding: 0;
+    border-radius: 50px;
+    font-size: 35px;
+    font-weight: 500;
+    color: #E4E4E4;
+    background-color: #393939;
+  )");
+
+  btn.setFixedSize(150, 100);
+
+  QObject::connect(&btn, &QPushButton::clicked, [=]() {
+    if (btn.text() == "UNSET") {
+      if (ConfirmationDialog::confirm("Do you want to unset?", this)) {
+        params.remove("CarModel");
+        combobox.setCurrentIndex(0);
+        refresh();
+      }
+    }
+  });
+
+  //combobox.view()->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+  hlayout->addWidget(&combobox);
+  hlayout->addWidget(&btn, Qt::AlignRight);
+
+  QObject::connect(&combobox, static_cast<void(QComboBox::*)(int)>(&QComboBox::activated), [=](int index)
+  {
+    combobox.itemData(combobox.currentIndex());
+    QString str = combobox.currentText();
+    if (combobox.currentIndex() != 0) {
+      if (ConfirmationDialog::confirm("Press OK to set your car as\n" + str, this)) {
+        params.put("CarModel", str.toStdString());
+      }
+    }
+    refresh();
+  });
+  refresh();
+}
+
+void CarSelectCombo::refresh() {
+  QString selected_carname = QString::fromStdString(params.get("CarModel"));
+  int index = combobox.findText(selected_carname);
+  if (index >= 0) combobox.setCurrentIndex(index);
+  if (selected_carname.length()) {
+    btn.setEnabled(true);
+    btn.setText("UNSET");
+  } else {
+    btn.setEnabled(false);
+    btn.setText("SET");
+  }
+}
---
+++ selfdrive/ui/qt/widgets/opkr.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <QPushButton>
+
+#include "selfdrive/hardware/hw.h"
+#include "selfdrive/ui/qt/widgets/controls.h"
+#include <QComboBox>
+#include <QAbstractItemView>
+
+class SshLegacyToggle : public ToggleControl {
+  Q_OBJECT
+
+public:
+  SshLegacyToggle() : ToggleControl("Use Legacy SSH Key", "", "", Params().getBool("OpkrSSHLegacy")) {
+    QObject::connect(this, &SshLegacyToggle::toggleFlipped, [=](int state) {
+      bool status = state ? true : false;
+      Params().putBool("OpkrSSHLegacy", status);
+    });
+  }
+};
+
+class CarSelectCombo : public AbstractControl 
+{
+  Q_OBJECT
+
+public:
+  CarSelectCombo();
+
+private:
+  QPushButton btn;
+  QComboBox combobox;
+  Params params;
+
+  void refresh();
+};
+
+class PrebuiltToggle : public ToggleControl {
+  Q_OBJECT
+
+public:
+  PrebuiltToggle() : ToggleControl("Use Prebuilt", "", "", Params().getBool("PutPrebuiltOn")) {
+    QObject::connect(this, &PrebuiltToggle::toggleFlipped, [=](int state) {
+      bool status = state ? true : false;
+      Params().putBool("PutPrebuiltOn", status);
+    });
+  }
+};
