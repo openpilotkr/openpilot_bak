fc28f6022166951cb14c0943cb37afed460d185e
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-18 23:10:35 +0900

    comma

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -178,7 +178,7 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
       // enter controls on rising edge of ACC, exit controls on ACC off
       if (addr == 1057) {
         // 2 bits: 13-14
-        int cruise_engaged = (GET_BYTES_04(to_push) >> 13) & 0x1;
+        int cruise_engaged = (GET_BYTES_04(to_push) >> 14) & 0x1;
         if (cruise_engaged && !cruise_engaged_prev) {
           controls_allowed = 1;
         }
