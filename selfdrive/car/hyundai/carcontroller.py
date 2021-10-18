8cfcf439bf04353b9b49de77cd658e25cd925e04
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-18 19:07:30 +0900

    kyd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -188,7 +188,7 @@ class CarController():
     self.sm = messaging.SubMaster(['controlsState', 'radarState'])
 
   def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, visual_alert,
-             left_lane, right_lane, left_lane_depart, right_lane_depart, set_speed, lead_visible, lead_dist, lead_vrel, lead_yrel, v_future, sm):
+             left_lane, right_lane, left_lane_depart, right_lane_depart, set_speed, lead_visible, lead_dist, lead_vrel, lead_yrel, v_future):
 
     # *** compute control surfaces ***
 
