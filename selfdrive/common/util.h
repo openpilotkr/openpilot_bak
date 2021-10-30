cf614fc77abfcf6425284c5d0c2eff30e1b7247a
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-31 01:58:56 +0900

    kyd

--- selfdrive/common/util.h
+++ selfdrive/common/util.h
@@ -13,6 +13,7 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <vector>
 
 // keep trying if x gets interrupted by a signal
 #define HANDLE_EINTR(x)                                       \
@@ -32,7 +33,7 @@ typedef void (*sighandler_t)(int sig);
 void set_thread_name(const char* name);
 
 int set_realtime_priority(int level);
-int set_core_affinity(int core);
+int set_core_affinity(std::vector<int> cores);
 
 namespace util {
 
