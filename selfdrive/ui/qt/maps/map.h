6b727f6eda9d5c520991c17be8e124f41d8ed068
multikyd <72329880+multikyd@users.noreply.github.com>
2021-10-31 22:40:03 +0900

    kyd

--- selfdrive/ui/qt/maps/map.h
+++ selfdrive/ui/qt/maps/map.h
@@ -28,8 +28,8 @@
 #include "cereal/messaging/messaging.h"
 
 //const QString MAPBOX_TOKEN = util::getenv("MAPBOX_TOKEN").c_str();
-const QString MAPBOX_TOKEN = QString::fromStdString(Params().get("dp_mapbox_token_sk"));
-const QString MAPS_HOST = util::getenv("MAPS_HOST", MAPBOX_TOKEN.isEmpty() ? "https://maps.comma.ai" : "https://api.mapbox.com").c_str();
+//const QString MAPBOX_TOKEN = QString::fromStdString(Params().get("dp_mapbox_token_sk"));
+//const QString MAPS_HOST = util::getenv("MAPS_HOST", MAPBOX_TOKEN.isEmpty() ? "https://maps.comma.ai" : "https://api.mapbox.com").c_str();
 
 class MapInstructions : public QWidget {
   Q_OBJECT
