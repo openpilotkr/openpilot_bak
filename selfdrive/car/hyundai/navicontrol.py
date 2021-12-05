e5087da03f4a84cd28a081dd5a6979827d5835fd
multikyd <72329880+multikyd@users.noreply.github.com>
2021-12-06 08:52:46 +0900

    kyd

--- selfdrive/car/hyundai/navicontrol.py
+++ selfdrive/car/hyundai/navicontrol.py
@@ -157,7 +157,7 @@ class NaviControl():
           cruise_set_speed_kph = spdTarget + self.map_spdlimit_offset
         if cruise_set_speed_kph+1 < v_ego_mph and CS.is_set_speed_in_mph and not CS.out.gasPressed:
           self.onSpeedControl = True
-        elif cruise_set_speed_kph+1 < v_ego_kph and not CS.out.gasPressed:
+        elif cruise_set_speed_kph+1 < v_ego_kph and not CS.is_set_speed_in_mph and not CS.out.gasPressed:
           self.onSpeedControl = True
         else:
           self.onSpeedControl = False
