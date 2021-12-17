e62cc4fde9c45a456cd6b25b04d5282da0be4f62
multikyd <72329880+multikyd@users.noreply.github.com>
2021-12-18 02:42:22 +0900

    kyd

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -599,13 +599,14 @@ void OnroadHud::paintEvent(QPaintEvent *event) {
     }
   }
 
-  QRect rec_btn(rect().right() - bdr_s - 140 - 20, 905, 140, 140);
+  // opkr rec
+  QRect recbtn_draw(rect().right() - bdr_s - 140 - 20, 905, 140, 140);
   p.setBrush(Qt::NoBrush);
   if (rec_stat) p.setBrush(QColor(255, 0, 0, 150));
   p.setPen(QPen(QColor(255, 255, 255, 80), 6));
-  p.drawEllipse(rec_btn);
+  p.drawEllipse(recbtn_draw);
   p.setPen(QColor(255, 255, 255, 200));
-  p.drawText(rec_btn, Qt::AlignCenter, QString("REC"));
+  p.drawText(recbtn_draw, Qt::AlignCenter, QString("REC"));
   dashcam(s);
 
 }
