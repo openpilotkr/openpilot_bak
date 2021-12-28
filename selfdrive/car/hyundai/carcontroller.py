7d068b43da28069cc5c2e6dc36c4a105aeed8db0
multikyd <72329880+multikyd@users.noreply.github.com>
2021-12-28 11:32:54 +0900

    kyd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -530,22 +530,28 @@ class CarController():
         aReqValue = CS.scc12["aReqValue"]
         accel = actuators.accel if enabled else 0
         #accel = clip(accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
-        if 0 < CS.out.radarDistance <= 149 and self.radar_helper_option == 1:
+        if 0 < CS.lead_distance <= 149 and self.radar_helper_option == 1:
           # neokii's logic, opkr mod
           stock_weight = 0.
           if aReqValue > 0.:
-            stock_weight = interp(CS.out.radarDistance, [3.5, 17.0, 25.0], [0.7, 1.0, 0.0])
+            stock_weight = interp(CS.lead_distance, [3.5, 17.0, 30.0], [0.7, 1.0, 0.0])
           elif aReqValue < 0. and self.stopping_dist_adj_enabled:
-            stock_weight = interp(CS.out.radarDistance, [2.0, 4.5, 6.5, 25.0], [1.0, 0.2, 1.0, 0.0])
+            stock_weight = interp(CS.lead_distance, [2.0, 4.5, 5.5, 30.0], [1.0, 0.3, 1.0, 0.0])
           elif aReqValue < 0.:
-            stock_weight = interp(CS.out.radarDistance, [3.5, 25.0], [1.0, 0.0])
+            stock_weight = interp(CS.lead_distance, [4.0, 30.0], [1.0, 0.0])
           else:
             stock_weight = 0.
           accel = accel * (1. - stock_weight) + aReqValue * stock_weight
-        elif 0 < CS.out.radarDistance <= 149 and self.radar_helper_option == 2:
-          accel = aReqValue
-        elif 0 < CS.out.radarDistance <= 4.0: # use radar by force to stop anyway below 3.5m if lead car is recognized.
-          stock_weight = interp(CS.out.radarDistance, [2.5, 3.5], [1., 0.])
+        elif 0 < CS.lead_distance <= 149 and self.radar_helper_option == 2:
+          if self.stopping_dist_adj_enabled:
+            if CS.clu_Vanz < 3 and 3.0 < CS.lead_distance < 5.5 and aReqValue < 0 and -5 < lead_objspd:
+              accel = aReqValue * interp(CS.lead_distance, [3.0, 4.5, 5.5], [1.0, 0.2, 1.0])
+            else:
+              accel = aReqValue
+          else:
+            accel = aReqValue
+        elif 0 < CS.lead_distance <= 4.0: # use radar by force to stop anyway below 4.0m if lead car is detected.
+          stock_weight = interp(CS.lead_distance, [2.5, 4.0], [1., 0.])
           accel = accel * (1. - stock_weight) + aReqValue * stock_weight
         else:
           stock_weight = 0.
